60d35c36d10d8d8416b767204949c85c0f26c4e8
The_Sushi <64918853+Dev-Atsushi@users.noreply.github.com>
2021-02-02 10:18:22 -0300

    🍩・ Montando a config.

---
+++ config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  token: "XXXXXXXXXXX", // Token do seu bot.
+  
+  clientsecret: "XXXXX", // ClientSecret do seu bot.
+  clientid: "XXXXXXXX", // Id do seu bot
+  url: "XX", // Url do seu site sem os https://.
+  
+  bot: {
+    botname: "XXXX", // Nome do bot
+    botlogolink:
+      "XXXXXXX", // Logo do bot em link.
+    botdescription:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", // Descrição do bot.
+    creatorname: "XXXXX#0000", // Nome do criador com tag.
+    supportserverinvite: "XXXXXXXX", // Convite do server de suporte
+    botinvitelink:
+      "XXXXXXXXXXXXXXXXXX", // Convite do bot.
+    prefix: "XX", // Prefixo do bot.
+    language: "XXXXX" // Lingua do bot.
+  },
+  
+  path: {
+    "commands": "./commands",
+    "events": "./events" 
+  }
+};
