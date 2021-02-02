bc5f19c61340e39ce1ee8089f188ad1b6c9abf18
The_Sushi <64918853+Dev-Atsushi@users.noreply.github.com>
2021-02-02 11:46:23 -0300

    💅・Carregando no emojis.

--- commands/dashboard.js
+++ commands/dashboard.js
@@ -18,9 +18,9 @@ const Base = require("../base/Command");
     let embed = new Discord.MessageEmbed()
       .setColor("BLACK")
       .setAuthor(message.client.config.bot.botname, message.client.user.displayAvatarURL())
-      .setTitle("<:lovekanji:776966204815704116> | Configure seu servidor:")
-      .setDescription("Entre em meu website para customizar seu servidor.\n [Clique aqui para entrar na dashboard](https://hiroshi.tk) ・ <a:estrelastar:769621593122537512>")
-      .addField('<:rikka_warn:777512813068419134> | Permissão?', '**`Dono do servidor`**', true)
+      .setTitle("Configure seu servidor:")
+      .setDescription("Entre em meu website para customizar seu servidor.\n [Clique aqui para entrar na dashboard](https://"+message.client.config.url+") ・ <a:estrelastar:769621593122537512>")
+      .addField('Permissão?', '**`Dono do servidor`**', true)
       .setFooter("Comando requesitado por "+message.author.username+"",  message.author.displayAvatarURL({ dynamic: true })) 
     message.channel.send(embed)
     
