d7e9da6f7f08db2b997974be3f1bc99ba3c59b38
The_Sushi <64918853+Dev-Atsushi@users.noreply.github.com>
2021-02-02 10:37:47 -0300

    🍩・Montando a config.

--- config.js
+++ config.js
@@ -1,35 +1,32 @@
 module.exports = {
-  token: "XXXXXXXXXXX", // Token do seu bot.
-  
-  clientsecret: "XXXXX", // ClientSecret do seu bot.
-  clientid: "XXXXXXXX", // Id do seu bot
-  url: "XX", // Url do seu site sem os https://.
-  
-  api: {
-    dbl: "XXXX" // https://discordbots.org/api/docs#mybots // Pegando o key da api.
-  },
-  
-  votes: {
-		port: 5000, // Porta da dbl
-		password: "XXXXXXXXXXX" // Password do bot dbl
-	},
-  
-  bot: {
-    botname: "XXXX", // Nome do bot
-    botlogolink:
-      "XXXXXXX", // Logo do bot em link.
-    botdescription:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", // Descrição do bot.
-    creatorname: "XXXXX#0000", // Nome do criador com tag.
-    supportserverinvite: "XXXXXXXX", // Convite do server de suporte
-    botinvitelink:
-      "XXXXXXXXXXXXXXXXXX", // Convite do bot.
-    prefix: "XX", // Prefixo do bot.
-    language: "XXXXX" // Lingua do bot.
-  },
-  
-  path: {
-    "commands": "./commands",
-    "events": "./events" 
-  }
+    token: "XXXXXXXXXXX", // Token do seu bot.
+
+    clientsecret: "XXXXX", // ClientSecret do seu bot.
+    clientid: "XXXXXXXX", // Id do seu bot
+    url: "XX", // Url do seu site sem os https://.
+
+    api: {
+        dbl: "XXXX" // https://discordbots.org/api/docs#mybots // Pegando o key da api.
+    },
+
+    vote: {
+        port: 5000, // Porta da dbl
+        password: "XXXXXXXXXXX" // Password do bot dbl
+    },
+
+    bot: {
+        botname: "XXXX", // Nome do bot
+        botlogolink: "XXXXXXX", // Logo do bot em link.
+        botdescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", // Descrição do bot.
+        creatorname: "XXXXX#0000", // Nome do criador com tag.
+        supportserverinvite: "XXXXXXXX", // Convite do server de suporte
+        botinvitelink: "XXXXXXXXXXXXXXXXXX", // Convite do bot.
+        prefix: "XX", // Prefixo do bot.
+        language: "XXXXX" // Lingua do bot.
+    },
+
+    path: {
+        "commands": "./commands",
+        "events": "./events"
+    }
 };
